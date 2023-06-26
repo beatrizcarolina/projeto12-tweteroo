60a69b8082b6bb658f37d06dad25ad29e176cd69
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-06-26 11:34:36 -0300

    create post /sign-up on app.js

--- src/app.js
+++ src/app.js
@@ -6,5 +6,18 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = 5000;
+let users = [];
+
+app.post('/sign-up', (req, res) => {
+  const user = req.body;
+  console.log(user);
+
+  if(user.username === "" || user.avatar === "" || typeof user.username !== "string" || typeof user.avatar !== "string"){
+    res.status(400).json({message:"Todos os campos são obrigatórios!"});
+  } else {
+    users.push(user);
+    res.status(200).json({ message: "OK" });
+  }
+});
 
 app.listen(PORT,  () => console.log(`Running server on port ${PORT}`));
